5e6997e117f8412ff6000f067a5159049398ecb5
Rudransh Gupta <rudrag0101@gmail.com>
2022-12-02 23:52:27 +0530

    Pushing the React app
    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This website is under construction. Come back later!
+        </p>
+        <a
+          className="App-link"
+          href="https://gds.sntiitk.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit the previous site till then.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
